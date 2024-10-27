05f01e4ea06d67f0221b09336f70c7a5e4c30e9d
Graham Arnett <GrahamKentArnett@outlook.com>
2024-10-26 23:19:37 -0400

    tries now actually update, still fixing it though

--- Guessing Game App/Program.cs
+++ Guessing Game App/Program.cs
@@ -8,60 +8,92 @@
             Random random = new Random();
             String choice;
             int answer = random.Next(1, 101);//1 higher than 100 because the upper band is exclusive
-            do 
+            do
             {
                 Console.WriteLine("I'm thinking of a number from 1 to 100." +
                     "\nTry to guess it.");
-                Console.Write("Enter your number: ");
-                int entry = int.Parse(Console.ReadLine());
-                
+
+                //Console.Write("Enter your number: ");
+                //int entry = int.Parse(Console.ReadLine());
+
                 int tries = 0;
-                int difference = Math.Abs(answer - entry);
-                if (entry == answer)
+                bool correctGuess = false;
+                //int difference = Math.Abs(answer - entry);
+                while (!correctGuess)
                 {
-                    Console.WriteLine("Fantastic! You got it right!");
-                    Console.WriteLine($"You got it right in {tries} guesses!\n");
-                    Console.ForegroundColor = ConsoleColor.Green;
-                }
-                else if(entry > answer)
-                {
-                    if (difference > 10)
+                    Console.Write("Enter your number: ");
+                    //int entry = int.Parse(Console.ReadLine());
+
+                    if (int.TryParse(Console.ReadLine(), out int entry))
                     {
-                        Console.WriteLine("You are way too high!");
                         tries++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Too high");
-                        tries++;
+                        int difference = Math.Abs(answer - entry);
+                        if (entry == answer)
+                        {
+                            if (tries <= 3)
+                            {
+                                Console.WriteLine("Fantastic! You got it right!");
+                                Console.WriteLine($"You got it right in {tries} guesses!\n");
+                            }
+                            else if (tries >3 && tries <= 7)
+                            {
+                                Console.WriteLine("Not bad!");
+                                Console.WriteLine($"You got it right in {tries} guesses!\n");
+                            }
+                            else if (tries > 7 && tries <= 10)
+                            {
+                                Console.WriteLine("You might be a numbskull.");
+                                Console.WriteLine($"You got it right in {tries} guesses!\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Yeah, definitely a numbskull.");
+                                Console.WriteLine($"You got it right in {tries} guesses!\n");
+                            }
+                        }
+                        else if (entry > answer)
+                        {
+                            if (difference > 10)
+                            {
+                                Console.WriteLine("You are way too high!");
+                             //   tries++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Too high");
+                               // tries++;
+                            }
+                        }
+                        else if (entry < answer)
+                        {
+                            if (difference > 10)
+                            {
+                                Console.WriteLine("You are way too low!");
+                               // tries++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Too low");
+                               // tries++;
+                            }
+                        }
+                        else if (entry < 1 || entry > 100)
+                        {
+                            Console.WriteLine("I literally told you to enter something between 1 and 100.\n");
+                            //tries++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter a valid number! >:(\n");
+                            //tries++;
+                        }
                     }
                 }
-                else if (entry < answer)
-                {
-                    if (difference > 10)
-                    {
-                        Console.WriteLine("You are way too low!");
-                        tries++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Too low");
-                        tries++;
-                    }
-                }
-                else if (entry < 1 || entry > 100)
-                {
-                    Console.WriteLine("I literally told you to enter something between 1 and 100.\n");
-                    tries++;
-                }
-                else
-                {
-                    Console.WriteLine("You got it wrong! >:(\n");
-                    tries++;
-                }
-                Console.Write("Would you like to go again? (y/n): ");
-                choice = Console.ReadLine().ToLower();
-            }while(choice == "y");
-        }
+                    Console.Write("Would you like to go again? (y/n): ");
+                    choice = Console.ReadLine().ToLower();
+                } while (choice == "y") ;
+                Console.WriteLine("\nGoodbye, thanks for playing!");
+            }
     }
-}
+    }
+
